1c4885835f8c74efb4c9cb51c02d8ed4a10a710b
tldxdy <747417048@qq.com>
2016-02-29 18:17:30 +0800

    Revert "lq"
    This reverts commit d63776fa45599ec1fd3a2aceca6cd5736301e944.

--- Qilaiqiqu/src/com/qizhi/qilaiqiqu/activity/BaseActivity.java
+++ Qilaiqiqu/src/com/qizhi/qilaiqiqu/activity/BaseActivity.java
@@ -9,29 +9,18 @@ import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
 import android.app.Activity;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Intent;
 import android.content.res.AssetManager;
-import android.support.v4.app.NotificationCompat;
 
-import com.easemob.chat.EMMessage;
-import com.easemob.chat.EMMessage.Type;
-import com.easemob.util.EasyUtils;
 import com.qizhi.qilaiqiqu.model.CityModel;
 import com.qizhi.qilaiqiqu.model.DistrictModel;
 import com.qizhi.qilaiqiqu.model.ProvinceModel;
 import com.qizhi.qilaiqiqu.service.XmlParserHandler;
-import com.qizhi.qilaiqiqu.utils.CommonUtils;
 
 /**
  * @author leiqian 地图基类
  */
 public class BaseActivity extends Activity {
 
-	private static final int notifiId = 11;
-	protected NotificationManager notificationManager;
 	/**
 	 * ����ʡ
 	 */
@@ -136,40 +125,5 @@ public class BaseActivity extends Activity {
 
 		}
 	}
-	
-	
-	/**
-     * 当应用在前台时，如果当前消息不是属于当前会话，在状态栏提示一下 如果不需要，注释掉即可
-     * 
-     * @param message
-     */
-    protected void notifyNewMessage(EMMessage message) {
-        // 如果是设置了不提醒只显示数目的群组(这个是app里保存这个数据的，demo里不做判断)
-        // 以及设置了setShowNotificationInbackgroup:false(设为false后，后台时sdk也发送广播)
-        if (!EasyUtils.isAppRunningForeground(this)) {
-            return;
-        }
-
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(
-                this).setSmallIcon(getApplicationInfo().icon)
-                .setWhen(System.currentTimeMillis()).setAutoCancel(true);
-
-        String ticker = CommonUtils.getMessageDigest(message, this);
-        if (message.getType() == Type.TXT)
-            ticker = ticker.replaceAll("\\[.{2,3}\\]", "[表情]");
-        // 设置状态栏提示
-        mBuilder.setTicker(message.getFrom() + ": " + ticker);
-
-        // 必须设置pendingintent，否则在2.3的机器上会有bug
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, notifiId,
-                intent, PendingIntent.FLAG_ONE_SHOT);
-        mBuilder.setContentIntent(pendingIntent);
-
-        Notification notification = mBuilder.build();
-        notificationManager.notify(notifiId, notification);
-        notificationManager.cancel(notifiId);
-    }
 
 }
